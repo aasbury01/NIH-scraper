9bba9d6f32a662ae89f73652211cc4c9b7fcc8de
aasbury01 <alejandra.asbury@nikon.com>
2026-03-16 12:05:04 -0400

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -172,5 +172,6 @@ def secondary():
 
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
